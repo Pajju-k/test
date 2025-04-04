d81f7ce2e49c35ec933c7d2e68afaa3af205d82a
Pajju-k <imprajwalks@gmail.com>
2025-04-04 08:44:03 +0530

    save a filee

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 class test{
     public static void main(String[] args){
         System.out.println("java Class");
+        System.out.println("Devoops Class");
+        System.out.println("Devoops lab");
     }
 }
